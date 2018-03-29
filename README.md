765940d9af4102364fb79e6f4493c4d0cb06cd87
Owen Walpole <owengithub@gmail.com>
2018-03-28 22:15:04 -0500

    Bring to a playable state

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Minesweeper-Redone
 
 A (subjectively) better implementation of @bdholder's Minesweeper, WIP as of 03/27/2017.
+
+## TODO:
+- pseudocode for solver
+- documentation
+- FIXME/TODOs
+- tk
+- interactive mode
--- minesweeper.py
+++ minesweeper.py
@@ -1,76 +1,58 @@
-#!/usr/bin/python3
-
-from collections import namedtuple
+from enum import Enum
+from random import randrange
 
 # TODO: move this somewhere else
 # TODO: autodetect this
 
 # Do we want to use unicode icons?
-USE_UNICODE = True
+USE_UNICODE = False
 
-class _Symbols():
-    """Conveniance class for using getattr()."""
-    def __init__(self):
-        if USE_UNICODE:
-            self.ZERO    = "□"
-            self.ONE     = "①"
-            self.TWO     = "②"
-            self.THREE   = "③"
-            self.FOUR    = "④"
-            self.FIVE    = "⑤"
-            self.SIX     = "⑥"
-            self.SEVEN   = "⑦"
-            self.EIGHT   = "⑧"
-            
-            self.FLAG    = "🚩"
-            self.MINE    = "💣"
-            self.UNKNOWN = "�"
-        else:
-            self.ZERO    = "0"
-            self.ONE     = "1"
-            self.TWO     = "2"
-            self.THREE   = "3"
-            self.FOUR    = "4"
-            self.FIVE    = "5"
-            self.SIX     = "6"
-            self.SEVEN   = "7"
-            self.EIGHT   = "8"
+_ENGLISH = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight"
+}
 
-            self.FLAG    = "F"
-            self.MINE    = "*"
-            self.UNKNOWN = "?"
+class Symbols(Enum):
+    """Symbols used to denote cells."""
+
+    if USE_UNICODE:
+        ZERO    = "□"
+        ONE     = "①"
+        TWO     = "②"
+        THREE   = "③"
+        FOUR    = "④"
+        FIVE    = "⑤"
+        SIX     = "⑥"
+        SEVEN   = "⑦"
+        EIGHT   = "⑧"
+        
+        FLAG    = "🚩"
+        MINE    = "💣"
+        UNKNOWN = "�"
+    else:
+        ZERO    = "0"
+        ONE     = "1"
+        TWO     = "2"
+        THREE   = "3"
+        FOUR    = "4"
+        FIVE    = "5"
+        SIX     = "6"
+        SEVEN   = "7"
+        EIGHT   = "8"
+
+        FLAG    = "F"
+        MINE    = "*"
+        UNKNOWN = "?"
 
 class Cell():
     """A representation of a cell in a game of minesweeper."""
-
-    @staticmethod
-    def num2eng(num):
-        """Return an English string representation of num.
-        Args:
-            num: number to translate, 0 <= num <= 8
-            
-        Raises:
-            IndexError: if num is out of bounds"""
-        if num > 8 or num < 0:
-            raise IndexError
-        else:
-            return {
-                    0: "zero",
-                    1: "one",
-                    2: "two",
-                    3: "three",
-                    4: "four",
-                    5: "five",
-                    6: "six",
-                    7: "seven",
-                    8: "eight"
-                   }[num]
-
-    #@classmethod
-    #def __new__(cls):
-        #"""Wrapper around collections.namedtuple."""
-        #self = super(Cell, cls).__new__(cls, "Cell", "row", "column")
-        #return self
 
     def __init__(self):
         #FIXME
@@ -83,22 +65,180 @@ class Cell():
     def __str__(self):
 
         if self.flagged:
-            return FLAG
-        if self.has_mine:
-            return MINE
+            return Symbols.FLAG.value
         if not self.revealed:
-            return UNKNOWN
-        else:
-            # disgusting
-            return getattr(_Symbols(), self.num2eng(self.adjacent_mines).upper())
+            return Symbols.UNKNOWN.value
+        if self.has_mine:
+            return Symbols.MINE.value
+        # disgusting
+        return getattr(Symbols, _ENGLISH[self.adjacent_mines].upper()).value
 
-    def __repr__(self):
+#    def __repr__(self):
+#
+#        if self.revealed:
+#            return str(self.adjacent_mines)
+#        else:
+#            return ""
 
-        if self.revealed:
-            return str(self.adjacent_mines)
-        else:
-            return ""
+    # Because assignments aren't allowed in lambdas
+    def _mine(self):
+        self.has_mine = True
 
     def status(self):
         """Alias to self.__str__()."""
-        return self.__str___()
+
+        return self.__str__()
+
+class Minesweeper():
+    """An instance of the game."""
+
+    def _test_bounds(self, r, c):
+        """Check that (r, c) is within the board."""
+
+        if r < 0 or r >= self.rows:
+            raise IndexError
+        if c < 0 or c >= self.columns:
+            raise IndexError
+
+    def _test_bounds_nonfatal(self, r, c):
+        """Check that (r, c) is within the board."""
+
+        if r < 0 or r >= self.rows:
+            return False
+        if c < 0 or r >= self.columns:
+            return False
+        return True
+    
+    def __init__(self, rows, columns, mines=None):
+        """Creates a new instance of a Minesweeper game.
+        
+        Args:
+            rows: the number of rows on the board
+            columns: the number of columns on the board
+            mines: the number of mines on the board
+        """
+        
+        if rows <= 0:
+            raise IndexError("Rows must be greater than zero")
+        if columns <= 0:
+            raise IndexError("Columns must be greater than zero")
+        if mines is not None and mines >= rows * columns:
+            raise IndexError("Mines must be less than rows * columns")
+        
+        self.rows = rows
+        self.columns = columns
+        if mines is not None:
+            self.mines = mines
+        else:
+            self.mines = (rows * columns) // 3
+        self.generated = False
+        
+        self.board = [[Cell() for _ in range(rows)] for _ in range(columns)]
+
+        self.flags_placed = 0
+        self.mines_flagged = 0
+        self.result = None
+        
+    def __getitem__(self, coordinates):
+        
+        r, c = coordinates
+        self._test_bounds(r, c)
+        return self.board[r][c]
+
+#    def __setitem__(self, coordinates, value):
+#        
+#        r, c = coordinates
+#        self._test_bounds(r, c)
+#        self.board[r][c] = value
+
+    def flag(self, r, c):
+        """Flag (r, c) as a mine."""
+
+        self._test_bounds(r, c)
+        self[r, c].flagged = True
+        self.flags_placed += 1
+        if self[r, c].has_mine:
+            self.mines_flagged += 1
+
+    def unflag(self, r, c):
+        """Remove a flag from (r, c)."""
+
+        self._test_bounds(r, c)
+        self[r, c].flagged = False
+        self.flags_placed -= 1
+        if not self[r, c].has_mine:
+            self.mines_flagged -= 1
+    
+    def _count_adjacent_mines(self, r, c):
+        """Count mined cells adjacent to (r, c)."""
+
+        found_mines = 0
+        # gross!
+        for C in self._adjacents(r, c):
+            if self[C].has_mine:
+                found_mines += 1
+        return found_mines
+
+    def _adjacents(self, r, c):
+
+        return set((x, y) for x in {r - 1 if r > 0 else r, r, r + 1 if r < self.rows - 1 else r} for y in {c - 1 if c > 0 else c, c, c + 1 if c < self.columns - 1 else c} if (x, y) != (r, c))
+
+    
+    def game_over(self) -> bool:
+        """Is the game over?"""
+
+        return self.result is not None
+
+    def _place_mines(self, protected):
+
+        placed = 0
+        while placed < self.mines:
+            C = (randrange(self.rows), randrange(self.columns))
+            if not self[C].has_mine and C != protected:
+                self[C].has_mine = True
+                placed += 1
+                for D in self._adjacents(*C):
+                    self[D].adjacent_mines += 1
+
+    def reveal(self, r, c):
+        """Reveal the cell at (r, c)."""
+
+        if not self.generated:
+            self._place_mines((r, c))
+            self.generated = True
+            self.reveal(r, c)
+            return
+
+        if self[r, c].flagged:
+            return
+
+        self[(r, c)].revealed = True
+
+        if self[(r, c)].has_mine:
+            for i in range(self.rows):
+                for j in range(self.columns):
+                    if not self[(i, j)].revealed and self[(i, j)].has_mine:
+                        self.reveal(i, j)
+            self.result = False
+
+        if self[(r, c)].adjacent_mines == 0:
+            for C in self._adjacents(r, c):
+                if not self[C].revealed:
+                    self.reveal(*C)
+
+        if self.flags_placed == self.mines and self.mines_flagged == self.mines:
+            self.result = True
+
+    def __str__(self):
+
+        board = str()
+
+        for R in self.board:
+            for C in R:
+                board += "{} ".format(C)
+            board += "\n"
+
+        # TODO: generate cheeky messages
+        if self.game_over():
+            board += "Nice job, you won!" if self.result else "Sorry, you lost!"
+        return board
